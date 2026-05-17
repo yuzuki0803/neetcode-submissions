ed49efb02115a5382edd6ee618df508c898345e8
yuzuki0803 <nahida789@proton.me>
2026-05-17 14:48:25 +0530

    Add: design-hashset - submission-1

---
+++ Data Structures & Algorithms/design-hashset/submission-1.java
@@ -0,0 +1,28 @@
+class MyHashSet {
+
+    private final boolean[] table;
+    public MyHashSet() {
+        table = new boolean[1_000_000];
+    }
+    
+
+    public void add(int key) {
+        table[key] = true;
+    }
+    
+    public void remove(int key) {
+        table[key] = false;
+    }
+    
+    public boolean contains(int key) {
+        return table[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
